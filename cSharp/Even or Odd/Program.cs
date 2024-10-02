08c5152ca1224ea257a6ffc647cc191c77a1e803
mohamedelsaid056 <eng.mohamedelsaid1@gmail.com>
2024-10-02 22:19:25 +0300

    2-10

---
+++ cSharp/Even or Odd/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Even_or_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number to check if it's even or odd:");
+
+            // Improved input handling with TryParse
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Enter a valid number:");
+            }
+
+            CheckOddorEven(number);
+        }
+        public static void CheckOddorEven(int numbert)
+        {
+            if (numbert % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+
+    }
+}
---
+++ cSharp/README.md
@@ -0,0 +1,4 @@
+# problem_solving
+ problem_solving
+
+C# Program to Check Whether a Given Number is Even or Odd
